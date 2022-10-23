cb40556c51a9c1ef05ea4bc84cfda240861e2659
walter <54906654+agneske-arter-walter-mostertruck-firetr@users.noreply.github.com>
2022-10-23 22:04:49 +0000

    őph p has tohose bangarangs i belive

--- coder/linox.php
+++ coder/linox.php
@@ -1,5 +1,7 @@
+<?php
 $myFile = "rapsongs.db";
 $lines = file($myFile);//file in to an array
 foreach ($lines as $l){ //pleach is the sound a peach makes when hitting concret brick
 echo $l;
 }
+?>
